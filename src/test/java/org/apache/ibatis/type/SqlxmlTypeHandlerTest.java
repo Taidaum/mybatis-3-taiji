126b2d122bec63b9948fc65d0a3d3fbac92322c2
Taiji Uemura <110216942+Taidaum@users.noreply.github.com>
2023-06-30 16:48:53 -0300

    Update SqlxmlTypeHandlerTest.java

--- src/test/java/org/apache/ibatis/type/SqlxmlTypeHandlerTest.java
+++ src/test/java/org/apache/ibatis/type/SqlxmlTypeHandlerTest.java
@@ -173,25 +173,11 @@ class SqlxmlTypeHandlerTest extends BaseTypeHandlerTest {
     void insert(XmlBean bean);
   }
 
+  @Getter
+  @Setter
   public static class XmlBean {
     private Integer id;
 
     private String content;
-
-    public Integer getId() {
-      return id;
-    }
-
-    public void setId(Integer id) {
-      this.id = id;
-    }
-
-    public String getContent() {
-      return content;
-    }
-
-    public void setContent(String content) {
-      this.content = content;
-    }
   }
 }
